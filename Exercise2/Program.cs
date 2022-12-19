a345b4b4a396027b8d51252802ce102a4099cfdd
Irina Gorynina <iren.gorynina@gmail.com>
2022-12-19 22:20:45 +0300

    Example1

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 5;
+int b = 7;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
