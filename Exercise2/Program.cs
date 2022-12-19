68b54f6325434fefd40046886f014faf2717c3a2
Irina Gorynina <iren.gorynina@gmail.com>
2022-12-19 22:23:58 +0300

    Example2

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -8,3 +8,15 @@ if (b > max) max = b;
 
 Console.Write("max = ");
 Console.WriteLine(max);
+
+
+int c = 2;
+int d = 10;
+
+int max1 = c;
+
+if (c > max1) max1 = c;
+if (d > max1) max1 = d;
+
+Console.Write("max1 = ");
+Console.WriteLine(max1);
